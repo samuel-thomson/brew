a6546ea879cdf19686e2a932f6aaa942d2f3c2b3
Sam Thomson <samuelthomson62@gmail.com>
2020-01-07 12:03:45 -0500

    wrote out the formula for the next recipe in the interface

--- Brew/Models/StaticMethodLayer.cs
+++ Brew/Models/StaticMethodLayer.cs
@@ -9,7 +9,36 @@ namespace Brew.Models
     {
         public Recipe GetNextRecipe(Recipe r)
         {
-            
+            if(r.Extraction == "over")
+            {
+                if(r.Body == "heavy")
+                {
+                    //corsen up
+                    r.Grind += 1;
+                    return r;
+                }
+                else //if(r.Body == "thin")
+                {
+                    //up dose
+                    r.Dose += .5;
+                    return r;
+                }
+            }
+            else //if(r.Extraction == "under")
+            {
+                if(r.Body == "heavy")
+                {
+                    //down dose
+                    r.Dose -= .5;
+                    return r;
+                }
+                else //if(r.Body == "thin")
+                {
+                    //finer grind
+                    r.Grind -= 1;
+                    return r;
+                }
+            }
         }
     }
 }
